e3a2cb23f7cd195d108d62e324bb512f97a564e3
Abdul <metalsquad4u@gmail.com>
2022-09-20 22:04:19 +0300

    oidc commit

--- bin/github_oidc.ts
+++ bin/github_oidc.ts
@@ -4,18 +4,20 @@ import * as cdk from 'aws-cdk-lib';
 import { GithubOidcStack } from '../lib/github_oidc-stack';
 
 const app = new cdk.App();
+
+
+const projectname = 'aws-gh-oidc';
+const audience = 'sts.amazonaws.com'
+
 new GithubOidcStack(app, 'GithubOidcStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+  projectname,
+  allowedBranchPatternToPush: ['repo:metalsquad4u/GithubOIDC:refs:refs/heads/master'],
+  audience,
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env:{
+    region: 'us-east-1',
+    account: '683578897984',
+  }
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/github_oidc-stack.ts
+++ lib/github_oidc-stack.ts
@@ -1,16 +1,56 @@
 import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam'
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { ImagePullPrincipalType } from 'aws-cdk-lib/aws-codebuild';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class GithubOidcStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export interface GithubOidcStackProps extends StackProps {
+  projectname: string;
+  allowedBranchPatternToPush: string[];
+  audience: string;
+}
+
+export class GithubOidcStack extends Stack {
+  constructor(scope: Construct, id: string, props: GithubOidcStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { projectname, allowedBranchPatternToPush, audience } = props;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GithubOidcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const githubOIDCProvider = new iam.OpenIdConnectProvider(
+      this,
+      'GithubActions', {
+        url: 'https://token.actions.githubusercontent.com',
+        clientIds: ['sts.amazonaws.com'],
+      }
+    );
+/*
+    const s3Bucket = new s3.Bucket(this, 'my-bucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      //encryption: s3.BucketEncryption.KMS,
+      // 👇 encrypt with our KMS key
+     //encryptionKey: key,
+    });
+*/
+    const GithubActionsRole = new iam.Role(this, 'GithubActionsRole', {
+      assumedBy: new iam.WebIdentityPrincipal(
+        githubOIDCProvider.openIdConnectProviderArn,
+        {
+          StringEquals: {
+            'token.actions.githubusercontent.com:aud': audience,
+            'token.actions.githubusercontent.com.sub': allowedBranchPatternToPush,
+          },
+        }
+      ),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'),
+      ],
+      roleName: 'aws-gh-oidc',
+      description: `Role to assume from github actions pipeline of ${projectname}`,
+      maxSessionDuration: cdk.Duration.hours(1),
+    });
+
+
   }
 }
